405ea37878534304c6759af1a09223db39a98a17
Davide Sartor <davide.sartor.4@studenti.unipd.it>
2023-11-15 05:35:08 +0100

    add channel in condensation

--- mango/actions.py
+++ mango/actions.py
@@ -32,32 +32,39 @@ class GridCompatibility(ActionCompatibility):
     def __call__(
         self, comand: int, start_state: npt.NDArray, next_state: npt.NDArray
     ) -> float:
+        LEFT, DOWN, RIGHT, UP = 0, 1, 2, 3
         delta_y, delta_x = next_state - start_state
 
-        LEFT, DOWN, RIGHT, UP, STAY = 0, 1, 2, 3, -1
-        transition = {
-            (0, -1): LEFT,
-            (1, 0): DOWN,
-            (0, 1): RIGHT,
-            (-1, 0): UP,
-            (0, 0): STAY,
-        }.get((delta_y, delta_x), None)
+        if delta_y == 0 and delta_x == -1:
+            executed = LEFT
+        elif delta_y == 1 and delta_x == 0:
+            executed = DOWN
+        elif delta_y == 0 and delta_x == 1:
+            executed = RIGHT
+        elif delta_y == -1 and delta_x == 0:
+            executed = UP
+        else:
+            executed = None
 
-        if transition == comand:
+        if executed == comand:
             return 1.0
-        elif transition == STAY:
+        elif delta_y == 0 and delta_x == 0:
             return -0.1
         else:
             return -1.0
 
 
+@dataclass(frozen=True, eq=False)
 class CondensationCompatibility(GridCompatibility):
+    action_space = gym.spaces.Discrete(4)
+    channel: int = 0
+
     def __call__(
         self, comand: int, start_state: npt.NDArray, next_state: npt.NDArray
     ) -> float:
         grid_shape = start_state.shape[:-1]
-        start_idx = np.argmax(start_state[:, :, -1])
-        next_idx = np.argmax(next_state[:, :, -1])
+        start_idx = np.argmax(start_state[:, :, self.channel])
+        next_idx = np.argmax(next_state[:, :, self.channel])
         y_start, x_start = np.unravel_index(start_idx, grid_shape)
         y_next, x_next = np.unravel_index(next_idx, grid_shape)
         return super().__call__(
