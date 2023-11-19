1451a9c170b3e365db011cdffc8363f7081dc21a
Davide Sartor <davide.sartor.4@studenti.unipd.it>
2023-11-19 04:52:33 +0100

    done

--- mango/actions/abstract_actions.py
+++ mango/actions/abstract_actions.py
@@ -13,7 +13,9 @@ class AbstractActions(Protocol):
     def mask(self, obs: ObsType) -> ObsType:
         return obs
 
-    def beta(self, action: ActType, start_obs: ObsType, next_obs: ObsType) -> tuple[bool, bool]:
+    def beta(
+        self, action: ActType, start_obs: ObsType, next_obs: ObsType
+    ) -> tuple[bool, bool]:
         ...
 
     def compatibility(
@@ -31,41 +33,22 @@ class Grid2dActions(IntEnum):
 
 @dataclass(eq=False, slots=True, repr=True)
 class Grid2dMovement(AbstractActions):
+    action_space: ClassVar[spaces.Discrete] = spaces.Discrete(4)
     cell_shape: tuple[int, int]
     grid_shape: tuple[int, int]
-    agent_channel: Optional[int] = None
     p_termination: float = 0.1
     reward: float = 1.0
-    action_space: ClassVar[spaces.Discrete] = spaces.Discrete(4)
 
     def obs2coord(self, obs: ObsType) -> tuple[int, int]:
-        if self.agent_channel is None:
-            idx = obs[0] * self.grid_shape[1] + obs[1]
-        else:
-            idx = np.argmax(obs[self.agent_channel, :, :])
-        y, x = idx // self.grid_shape[1], idx % self.grid_shape[1]
+        y, x = obs
         return int(y // self.cell_shape[0]), int(x // self.cell_shape[1])
 
     def mask(self, obs: ObsType) -> ObsType:
-        if self.agent_channel is None:
-            return obs
+        return obs
 
-        padded_obs = np.zeros((obs.shape[0], obs.shape[1] + 2, obs.shape[2] + 2))
-        padded_obs[:, 1:-1, 1:-1] = obs
-
-        # add a channel with 0=padd, 1=valid
-        # padded_obs = np.zeros((obs.shape[0] + 1, obs.shape[1] + 2, obs.shape[2] + 2))
-        # padded_obs[:-1, 1:-1, 1:-1] = obs
-        # padded_obs[-1, 1:-1, 1:-1] = 1
-
-        y, x = self.obs2coord(obs)
-        y_min = y * self.cell_shape[0]
-        y_max = y_min + self.cell_shape[0] + 2
-        x_min = x * self.cell_shape[1]
-        x_max = x_min + self.cell_shape[1] + 2
-        return ObsType(padded_obs[:, y_min:y_max, x_min:x_max])
-
-    def beta(self, action: ActType, start_obs: ObsType, next_obs: ObsType) -> tuple[bool, bool]:
+    def beta(
+        self, action: ActType, start_obs: ObsType, next_obs: ObsType
+    ) -> tuple[bool, bool]:
         start_y, start_x = self.obs2coord(start_obs)
         next_y, next_x = self.obs2coord(next_obs)
         if start_y != next_y or start_x != next_x:
@@ -85,8 +68,12 @@ class Grid2dMovement(AbstractActions):
         next_y, next_x = self.obs2coord(next_obs)
         delta_y, delta_x = act2delta[Grid2dActions(action)]
         next_y_expected, next_x_expected = start_y + delta_y, start_x + delta_x
-        next_y_expected = max(0, min(self.grid_shape[0]//self.cell_shape[0] - 1, next_y_expected))
-        next_x_expected = max(0, min(self.grid_shape[1]//self.cell_shape[1] - 1, next_x_expected))
+        next_y_expected = max(
+            0, min(self.grid_shape[0] // self.cell_shape[0] - 1, next_y_expected)
+        )
+        next_x_expected = max(
+            0, min(self.grid_shape[1] // self.cell_shape[1] - 1, next_x_expected)
+        )
 
         if next_y == next_y_expected and next_x == next_x_expected:
             return self.reward
@@ -94,3 +81,31 @@ class Grid2dMovement(AbstractActions):
             return 0.0
         else:
             return -self.reward
+
+
+@dataclass(eq=False, slots=True, repr=True)
+class Grid2dMovementOnehot(Grid2dMovement):
+    agent_channel: int = 0
+    add_valid_channel: bool = False
+
+    def obs2coord(self, obs: ObsType) -> tuple[int, int]:
+        idx = np.argmax(obs[self.agent_channel, :, :])
+        y, x = idx // self.grid_shape[1], idx % self.grid_shape[1]
+        return int(y // self.cell_shape[0]), int(x // self.cell_shape[1])
+
+    def mask(self, obs: ObsType) -> ObsType:
+        if obs.shape[0] == 1:
+            return obs
+
+        if not self.add_valid_channel:
+            padded_obs = np.zeros((obs.shape[0], obs.shape[1] + 2, obs.shape[2] + 2))
+            padded_obs[:, 1:-1, 1:-1] = obs
+        else:
+            padded_obs = np.zeros((obs.shape[0] + 1, obs.shape[1] + 2, obs.shape[2] + 2))
+            padded_obs[:-1, 1:-1, 1:-1] = obs
+            padded_obs[-1, 1:-1, 1:-1] = 1
+
+        y, x = self.obs2coord(obs)
+        y_lims = (y * self.cell_shape[0], y * self.cell_shape[0] + self.cell_shape[0] + 2)
+        x_lims = (x * self.cell_shape[1], x * self.cell_shape[1] + self.cell_shape[1] + 2)
+        return ObsType(padded_obs[:, y_lims[0] : y_lims[1], x_lims[0] : x_lims[1]])
