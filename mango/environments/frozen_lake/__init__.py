062103b7caf1716ccd1a87b5075693d6dc06c47d
AlesArcudi <alessio.arcudi@studenti.unipd.it.com>
2023-11-22 17:10:04 +0100

    changes

--- mango/environments/frozen_lake/__init__.py
+++ mango/environments/frozen_lake/__init__.py
@@ -1,5 +1,5 @@
 from .utils import generate_map, reachable_from, random_board, sample_position_in
-from .plot_utils import plot_grid, plot_qval_heatmap, plot_trajectory, all_observations, get_qval
+from .plot_utils import plot_grid, plot_qval_heatmap, plot_trajectory, all_observations, get_qval, plot_all_qvals
 from .wrappers import ReInitOnReset, CoordinateObservation, TensorObservation, CustomFrozenLakeEnv, FrozenLakeEnv
 
 from ...actions.abstract_actions import Grid2dMovementOnehot as Actions
@@ -8,11 +8,14 @@ __all__ = ["generate_map",
            "reachable_from", 
            "random_board", 
            "sample_position_in", 
+           
            "plot_grid", 
            "plot_qval_heatmap", 
            "plot_trajectory", 
            "all_observations", 
-           "get_qval", 
+           "get_qval",
+           "plot_all_qvals",
+            
            "ReInitOnReset", 
            "CoordinateObservation", 
            "TensorObservation", 
