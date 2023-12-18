0cf98e5e50a02a1d50e2cce99e4c92725171d05d
davidesartor <davide.sartor.4@studenti.unipd.it>
2023-12-18 07:55:56 +0100

    move saved models to cpu

---
+++ frozen_lake_tests/run_sims_copy.py
@@ -0,0 +1,18 @@
+import os
+import sys
+
+# parameters for the environment
+map_scale = 3
+p_frozen = None
+
+if __name__ == "__main__":
+    cwd = os.getcwd()
+    sys.path.append(cwd)
+    import utils_save
+
+    dir_path = utils_save.path_to_save_dir(map_scale, p_frozen)
+    for filename in os.listdir(dir_path + "models/"):
+        normal_agent = utils_save.load_from_file(dir_path + "models/" + filename)
+        utils_save.save_to_file(path=dir_path + "models/" + filename, obj=normal_agent)
+        agent = utils_save.load_from_file(dir_path + "models/" + filename)
+        print(agent.policy.device)
