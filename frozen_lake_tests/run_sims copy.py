57155755119ce9d1f5d69b7175ba0819a0019666
Davide Sartor <davide.sartor.4@studenti.unipd.it>
2023-12-18 07:48:11 +0100

    koo

--- frozen_lake_tests/run_sims copy.py
+++ frozen_lake_tests/run_sims copy.py
@@ -7,8 +7,8 @@ import numpy as np
 # parameters for the environment
 map_scale = 2
 p_frozen = None
-device = torch.device("cuda:0")
-run_ids = [0]
+device = torch.device("cpu")
+run_ids = [0, 1, 2]
 train_normal_agent = True
 train_mango_agent = True
 
@@ -25,10 +25,11 @@ if __name__ == "__main__":
             if not filename.startswith("mango_agent"):
                 continue
             normal_agent = utils_save.load_from_file(dir_path + "models/" + filename)
-            normal_agent.policy.to(torch.device("cpu"))
             utils_save.save_to_file(
-                path=dir_path + f"normal_agent_run_{run_id}.pickle", obj=normal_agent
+                path=dir_path + "models/" + f"normal_agent_run_{run_id}.pickle", obj=normal_agent
             )
+            agent = utils_save.load_from_file(dir_path + "models/" + filename)
+            print(agent.policy.device)
 
         # load mango agent models one by one
         dir_path = utils_save.path_to_save_dir(map_scale, p_frozen)
@@ -36,10 +37,6 @@ if __name__ == "__main__":
             if not filename.startswith("mango_agent"):
                 continue
             mango_agent = utils_save.load_from_file(dir_path + "models/" + filename)
-            mango_agent.policy.to(torch.device("cpu"))
-            for layer in mango_agent.abstract_layers:
-                for policy in layer.policy.policies.values():
-                    policy.to(torch.device("cpu"))
             utils_save.save_to_file(
                 path=dir_path + f"mango_agent_run_{run_id}.pickle", obj=mango_agent
             )
