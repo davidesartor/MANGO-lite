ef5c8544b322a5c47306169d35cbb05cb596d33c
davidesartor <davide.sartor.4@studenti.unipd.it>
2024-01-03 19:43:00 +0100

    visualizations

--- frozen_lake_tests/utils_plot.py
+++ frozen_lake_tests/utils_plot.py
@@ -98,7 +98,9 @@ def plot_normal_agent_loss_reward(agent: Agent, save_path: Optional[str] = None)
 def plot_confront_loss_reward(
     agents: list[Mango | Agent], labels: list[str], save_path: Optional[str] = None
 ):
-    plt.figure(figsize=(8, 3))
+    plt.figure(figsize=(10, 4))
+    if save_path is not None:
+        plt.suptitle(save_path[-10:-4])
     plt.subplot(1, 2, 1)
     plt.title(f"reward")
     for agent, label in zip(agents, labels):
@@ -125,6 +127,79 @@ def plot_confront_loss_reward(
     plt.ylim((-0.1, None))
     plt.grid(True)
 
+    plt.legend(bbox_to_anchor=(1.05, 1), loc="upper left", borderaxespad=0.0)
+
+    if save_path is not None:
+        plt.savefig(save_path, bbox_inches="tight")
+    plt.show()
+
+
+def plot_confront_loss_reward_avg(
+    agents: list[list[Mango | Agent]],
+    labels: list[str],
+    colors=["tab:blue", "tab:orange"],
+    save_path: Optional[str] = None,
+):
+    def get_statistics(agents, eval=True):
+        reward_logs = []
+        ep_len_logs = []
+        for agent in agents:
+            reward_logs.append(smooth(agent.reward_log[1::2] if eval else agent.reward_log[::2]))
+            ep_len_logs.append(
+                smooth(agent.episode_length_log[1::2] if eval else agent.episode_length_log[::2])
+            )
+        max_len = max(len(r) for r in reward_logs)
+        reward_logs = [np.pad(r, (0, max_len - len(r)), mode="edge") for r in reward_logs]
+        reward_mean = np.mean(reward_logs, axis=0)
+        reard_ci95 = 1.96 * np.std(reward_logs, axis=0) / np.sqrt(len(reward_logs))
+        max_len = max(len(r) for r in ep_len_logs)
+        ep_len_logs = [np.pad(r, (0, max_len - len(r)), mode="edge") for r in ep_len_logs]
+        ep_len_mean = np.mean(ep_len_logs, axis=0)
+        ep_len_ci95 = 1.96 * np.std(ep_len_logs, axis=0) / np.sqrt(len(ep_len_logs))
+        return reward_mean, reard_ci95, ep_len_mean, ep_len_ci95
+
+    plt.figure(figsize=(10, 4))
+    if save_path is not None:
+        plt.suptitle(f"average across {len(agents[0])} runs")
+    for agent_list, label, color in zip(agents, labels, colors):
+        for eval in [True, False]:
+            r_mean, r_ci, ep_mean, ep_ci = get_statistics(agent_list, eval)
+            plt.subplot(1, 2, 1)
+            plt.title(f"reward")
+            plt.plot(
+                r_mean,
+                "-" if eval else ":",
+                color=color,
+                alpha=1 if eval else 0.5,
+                label=label + (" evaluation" if eval else " exploration"),
+            )
+            plt.fill_between(
+                range(len(r_mean)),
+                r_mean - r_ci,
+                r_mean + r_ci,
+                color=color,
+                alpha=0.3 if eval else 0.1,
+            )
+            plt.ylim((-0.05, 1.05))
+            plt.grid(True)
+            plt.subplot(1, 2, 2)
+            plt.title(f"episode lenght")
+            plt.plot(
+                ep_mean,
+                "-" if eval else ":",
+                color=color,
+                label=label + (" evaluation" if eval else " exploration"),
+                alpha=1 if eval else 0.5,
+            )
+            plt.fill_between(
+                range(len(ep_mean)),
+                ep_mean - ep_ci,
+                ep_mean + ep_ci,
+                color=color,
+                alpha=0.3 if eval else 0.1,
+            )
+    plt.ylim((-0.1, None))
+    plt.grid(True)
     plt.legend(bbox_to_anchor=(1.05, 1), loc="upper left", borderaxespad=0.0)
 
     if save_path is not None:
