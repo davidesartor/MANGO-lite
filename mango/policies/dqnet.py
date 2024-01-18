86a7d9f859e943b916f1b4b884090568f8ae17b8
davidesartor <davide.sartor.4@studenti.unipd.it>
2024-01-18 12:16:12 +0100

    test policy construction

--- mango/policies/dqnet.py
+++ mango/policies/dqnet.py
@@ -4,22 +4,13 @@ import numpy as np
 import torch
 
 from mango import spaces
-from mango.protocols import ActType, ObsType, Policy, StackedTransitions, Trainer, Transition
-from mango.neuralnetworks.networks import ConvEncoder
+from mango.protocols import ActType, ObsType, Policy, StackedTransitions, Trainer
 
 
 class DQNetPolicy(Policy):
     def __init__(self, net: torch.nn.Module, action_space: spaces.Discrete):
         self.action_space = action_space
         self.net = net
-
-    @classmethod
-    def make(
-        cls, action_space: spaces.Discrete, lr=1e-3, gamma=0.95, tau=0.005, **net_kwargs
-    ) -> tuple[Policy, Trainer]:
-        net = ConvEncoder(in_channels=None, out_features=int(action_space.n), **net_kwargs).eval()
-        trainer = DQNetTrainer(net, lr=lr, gamma=gamma, tau=tau)
-        return cls(net, action_space), trainer
 
     def get_action(self, obs: ObsType, randomness: float = 0.0) -> ActType:
         with torch.no_grad():
@@ -37,29 +28,32 @@ class DQNetPolicy(Policy):
 
 class DQNetTrainer(Trainer):
     def __init__(self, net: torch.nn.Module, lr=1e-3, gamma=0.95, tau=0.005):
+        self.lr = lr
         self.gamma = gamma
         self.tau = tau
         self.net = net
-        self.target_net = copy.deepcopy(net).train()
-        self.optimizer = torch.optim.RAdam(self.net.parameters(recurse=True), lr=lr)
 
     def train(self, transitions: StackedTransitions) -> float:
         if len(transitions) == 0:
             raise ValueError(f"{self.__class__.__name__}.train received empty transitions list")
-
         self.net.train()
         td = self.temporal_difference(transitions)
         loss = torch.nn.functional.smooth_l1_loss(td, torch.zeros_like(td))
+        self.soft_update()
         self.optimizer.zero_grad()
         loss.backward()
         self.optimizer.step()
-        self.soft_update()
         self.net.eval()
         return loss.item()
 
     def soft_update(self):
-        for target_param, param in zip(self.target_net.parameters(), self.net.parameters()):
-            target_param.data.copy_(self.tau * param.data + (1 - self.tau) * target_param.data)
+        # if has not target_net, copy net to target_net
+        if hasattr(self, "target_net"):
+            for target_param, param in zip(self.target_net.parameters(), self.net.parameters()):
+                target_param.data.copy_(self.tau * param.data + (1 - self.tau) * target_param.data)
+        else:
+            self.target_net = copy.deepcopy(self.net)
+            self.optimizer = torch.optim.RAdam(self.net.parameters(recurse=True), lr=self.lr)
 
     def temporal_difference(self, transitions: StackedTransitions) -> torch.Tensor:
         # double DQN - use qnet to select best action, use target_net to evaluate it
